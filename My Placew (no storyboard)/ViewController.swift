3c594ea2597f2552b8c6519c9e134f25b8f2d694
MaxLaurel <max.filippov.3333@gmail.com>
2022-11-10 16:42:30 +0300

    Initial Commit

---
+++ My Placew (no storyboard)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Placew (no storyboard)
+//
+//  Created by Максим on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
