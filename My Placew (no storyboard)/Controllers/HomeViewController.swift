fb0c72006b2b36d8abf5ffa8fd73eff7bd5aac27
MaxLaurel <max.filippov.3333@gmail.com>
2023-03-01 14:10:15 +0300

    second commit

--- My Placew (no storyboard)/Controllers/HomeViewController.swift
+++ My Placew (no storyboard)/Controllers/HomeViewController.swift
@@ -18,6 +18,7 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("new for commit")
         configureNavigationBar()
         configureTableView()
     }
