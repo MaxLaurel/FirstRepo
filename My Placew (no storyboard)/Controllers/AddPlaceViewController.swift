81ef54861d321b5b53f0a46cae841e0d468447d4
MaxLaurel <max.filippov.3333@gmail.com>
2023-03-11 22:02:56 +0300

    test commit

--- My Placew (no storyboard)/Controllers/AddPlaceViewController.swift
+++ My Placew (no storyboard)/Controllers/AddPlaceViewController.swift
@@ -66,7 +66,7 @@ class AddPlaceViewController: UIViewController {
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
     }
-    
+    print("for new mwrge")
     //    func tapGestureConfiguration() {
     //        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(thatWasTap))
     //        view.addGestureRecognizer(tapGesture)
